53905f98ed3cef7709fbad49ea4a2596378742e2
MrMaruf <abdirimov.maruf@gmail.com>
2024-06-03 10:45:02 +0200

    feat: moved stuff to main

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { existsSync, readFileSync, readdirSync, statSync, accessSync } from "node:fs";
+export function main() {
+    const srcPath = "./src";
+    readDir(srcPath);
+}
+
+
+console.log("test");
+
+function readDir(dirPath:string) {
+    try {
+        console.log("checking for access");
+        if (existsSync(dirPath)) {
+          console.log("access is possible. Reading...");
+          const dirStats = statSync(dirPath);
+          console.log(dirStats);
+          const files = readdirSync(dirPath);
+          console.log(files);
+          files.forEach((file) => {
+              readFile(`${dirPath}/${file}`);
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+}
+
+
+function readFile(filePath: string) {
+    try {
+        console.log(existsSync(filePath))
+        console.log("checking for access", accessSync(filePath));
+        const fileStats = statSync(filePath);
+        // const fileContents = readFileSync(filePath, 'utf8');
+        console.log(fileStats)
+        // console.log("File contents:", fileContents)
+    }
+    catch(error) {
+        console.error("File reading failed")
+        throw error;
+    }
+}
