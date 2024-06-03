b7f30487db0ebaa786128f2bc23d2165384995c1
MrMaruf <abdirimov.maruf@gmail.com>
2024-06-03 10:47:29 +0200

    feat: added prettier config

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,48 @@
-import { existsSync, readFileSync, readdirSync, statSync, accessSync } from "node:fs";
+import {
+    existsSync,
+    readFileSync,
+    readdirSync,
+    statSync,
+    accessSync,
+} from "node:fs";
 export function main() {
     const srcPath = "./src";
     readDir(srcPath);
 }
 
-
 console.log("test");
 
-function readDir(dirPath:string) {
+function readDir(dirPath: string) {
     try {
         console.log("checking for access");
         if (existsSync(dirPath)) {
-          console.log("access is possible. Reading...");
-          const dirStats = statSync(dirPath);
-          console.log(dirStats);
-          const files = readdirSync(dirPath);
-          console.log(files);
-          files.forEach((file) => {
-              readFile(`${dirPath}/${file}`);
-          });
+            console.log("access is possible. Reading...");
+            const dirStats = statSync(dirPath);
+            console.log(dirStats);
+            const files = readdirSync(dirPath);
+            console.log(files);
+            files.forEach((file) => {
+                if (file.includes(".")) {
+                    readFile(`${dirPath}/${file}`);
+                    continue;
+                }
+            });
         }
-      } catch (error) {
+    } catch (error) {
         console.error(error);
-      }
+    }
 }
-
 
 function readFile(filePath: string) {
     try {
-        console.log(existsSync(filePath))
+        console.log(existsSync(filePath));
         console.log("checking for access", accessSync(filePath));
         const fileStats = statSync(filePath);
         // const fileContents = readFileSync(filePath, 'utf8');
-        console.log(fileStats)
+        console.log(fileStats);
         // console.log("File contents:", fileContents)
-    }
-    catch(error) {
-        console.error("File reading failed")
+    } catch (error) {
+        console.error("File reading failed");
         throw error;
     }
 }
