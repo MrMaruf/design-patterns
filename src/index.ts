f99fd9496c544abe37b69131cd6e5adf6a9fe3ae
Maruf Abdirimov <marufabdirimov@studyportals.com>
2024-07-05 09:15:00 +0200

    cleanup: extracted the visitor files + working on arguments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-import { main } from "./main.js";
+import { main as visitorMain } from "./visitor/main.js";
 
-main();
+const argument = process.argv[2];
+
+switch (argument) {
+	case "visitor": {
+		visitorMain();
+		break;
+	}
+	case undefined: {
+		console.error("Visitor argument has not been provided"); 
+		break;
+	}
+	default: {
+		console.error("Unsupported argument was passed")
+		break;
+	}
+}
