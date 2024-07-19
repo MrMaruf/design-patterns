ca68e7440e21cd47a753babdd249384ea47a7760
MrMaruf <58888354+MrMaruf@users.noreply.github.com>
2024-07-19 16:31:39 +0200

    feat: added iterator execution

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import { main as visitorMain } from "./visitor/main.js";
+import { main as iteratorMain } from "./iterator/main.js";
 
 const argument = process.argv[2];
 
-console.info("Passed argument: ", argument)
+console.info("Passed argument: ", argument);
 
 switch (argument) {
-	case "visitor": {
-		visitorMain();
-		break;
-	}
-	case undefined: {
-		console.error("Visitor argument has not been provided"); 
-		break;
-	}
-	default: {
-		console.error("Unsupported argument was passed")
-		break;
-	}
+    case "visitor": {
+        visitorMain();
+        break;
+    }
+    case "iterator": {
+        iteratorMain();
+        break;
+    }
+    case undefined: {
+        console.error("Visitor argument has not been provided");
+        break;
+    }
+    default: {
+        console.error("Unsupported argument was passed");
+        break;
+    }
 }
